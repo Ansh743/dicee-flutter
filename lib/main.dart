3ebd88e2f225166faf53e47f77bf222c5eacf257
TheMuellenator <TheMuellenator@users.noreply.github.com>
2020-01-17 18:16:46 +0000

    Removing android.support dependencies Updating gradle wrapper to 5.4.1-all.zip Updating gradle build to 3.5.3 Adding AndroidX gradle properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.redAccent,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+              child: Text('Dicee',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontFamily: 'Pacifico'))),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
       ),
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceFace = 1;
+  var rightDiceFace = 1;
+
+  void changeFace() {
+    setState(() {
+      leftDiceFace = Random().nextInt(6) + 1;
+      rightDiceFace = Random().nextInt(6) + 1;
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              padding: EdgeInsets.all(5.0),
+              onPressed: () {
+                changeFace();
+              },
+              child: Image.asset('images/dice$leftDiceFace.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              padding: EdgeInsets.all(5.0),
+              onPressed: () {
+                changeFace();
+              },
+              child: Image.asset('images/dice$rightDiceFace.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
